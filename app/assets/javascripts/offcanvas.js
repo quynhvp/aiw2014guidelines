6499d56182fd297c17c553dc08d650c06f7a6335
Ha Quang Minh <minhprg@gmail.com>
2014-11-16 12:17:04 +0700

    Bootstrap and layout setup

---
+++ app/assets/javascripts/offcanvas.js
@@ -0,0 +1,8 @@
+/**
+ * Created by qha on 11/16/2014.
+ */
+$(document).ready(function () {
+    $('[data-toggle="offcanvas"]').click(function () {
+        $('.row-offcanvas').toggleClass('active')
+    });
+});
